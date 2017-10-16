f13db847aa38f66582d07132e3c62d3f4ebe3aef
Pannous <info@pannous.net>
2017-10-16 14:24:24 +0200

    netbase=require('netbase') merkel=netbase.get("Angela Merkel") assert(merkel.Vorname="Angela")

--- node-netbase.js
+++ node-netbase.js
@@ -1,181 +1,200 @@
-var child_process=require('child_process') 
-var system=child_process.execSync
-var ffi = require('ffi');
-var struct = require('ref-struct');
-var ref = require('ref')
-var ArrayType = require('ref-array') 
-// ArrayType(int)=int[] vs char**=ref.refType(charPtr);  var a = new ffi.Array('int', 10)
-// var wget = require('node-wget');
-// var addons = require('addons'); // for C++ structures, not needed here
-
-var SegfaultHandler = require('segfault-handler');// not always helpful 
-SegfaultHandler.registerHandler("crash.log", function(signal, address, stack) {//  before the signal is forwarded 
-  console.log(signal)
-  console.log(address)
-  console.log(stack)
-});
-
-// var cb = new ffi.Callback([ 'v', [ ffi.Array('short', 5), 'long' ] ], someFunc)
-
-VOID='void'
-BOOL=bool='bool'
-DOUBLE='double'
-INT=int='int'
-LONG=long='long'
-POINTER='pointer'
-STRING='string'
-// charPtr = ref.refType('char');
-intPtr=ref.refType('int')
-// StringArray = ArrayType('string');
-// charPtrPtr = ref.refType(charPtr);
-// CharPtrArray = ArrayType(charPtr);
-
-var Node0=struct( {
-    id:int,
-    name:STRING,
-    kind:int,
-    statementCount:int,
-    firstStatement:int ,
-    lastStatement:int,
-    value:POINTER ,
-  })
-Node=ref.refType(Node0)
-NodeList=ref.refType(Node)
-NodeArray=ArrayType(Node)
-// NodeVector='pointer' // not C compatible
-
-var Statement0=struct({
-    context:int ,
-    subject:int ,
-    predicate:int,
-    object:int,
-    nextSubjectStatement:int,
-    nextPredicateStatement:int,
-    nextObjectStatement:int,
-  })
-Statement=ref.refType(Statement0)
-
-netbase = ffi.Library(__dirname+'/lib/netbase', {
-  ceil: [ DOUBLE, [ DOUBLE ]/*,abi|async|varargs*/ ],
-  printf:[VOID,[STRING]] ,
-  allowWipe:[VOID,[]],
-  // test:[VOID,[]],
-  testAll:[VOID,[]],
-  setMemoryLimit:[LONG,[LONG]],
-  init:[VOID,[BOOL/*relations*/]], 
-  //////////
-  import:[VOID,[STRING/*type*/]],
-  execute:[ArrayType(Node),[STRING,intPtr]] , 
-  learn:[VOID,[STRING]] ,
-  // query:[VOID,[STRING]] ,
-  add:[Node,[STRING/* nodeName*/]],//, int kind
-  addStatement:[Statement,[Node,Node,Node]],
-  addStatement4:[Statement,[int,int,int]],
-  get:[Node,[int/*NodeId*/]],
-  getName:[STRING,[int/* node*/]],
-  getText:[STRING,[Node]],
-  getData:[POINTER,[int/* node*/]],
-  getAbstract:[Node,[STRING]],
-  getThe:[Node,[STRING]],
-  getProperty:[Node,[Node,STRING,int]],
-  getNew:[Node,[STRING]],
-  getStatementId:[int,[long/*pointer ?*/]],
-  hasNode:[BOOL,[STRING]],
-  // import:[VOID,[STRING/*type*/,STRING/*filename*/]],
-  setLabel:[VOID,[Node,STRING]],
-  setName:[VOID,[INT,STRING]],
-  deleteNode:[VOID,[INT]],
-  showNode:[VOID,[INT]],
-  deleteStatement:[VOID,[INT]],
-  has:[Node,[Node,Node]],
-  isA:[bool,[Node,Node]],
-});
-netbase.parse=netbase.execute
-netbase.getI=netbase.get
-netbase.get=function(id){
-  if(typeof id == "object" && id.kind)
-    return id
-  if(typeof id == "number")
-    return netbase.getI(id).deref();
-  else 
-    return netbase.getAbstract(id).deref()
-}
-function get(n) {
-  if(typeof n=='string')n=netbase.get(n);return n
-}
-Object.assign(Node.prototype, { 
-  show(){ netbase.show(this);console.log(this)},
-  save(){ netbase.setName(this.id,this.name);},// copy local string
-  delete(){ netbase.deleteNode(this.id)},
-  get(prop){return netbase.getProperty(this.ref(),prop,0).deref()},
-  is(typ){return netbase.isA(this.ref(),get(typ).ref())},
-  has(typ){z=netbase.has(this.ref(),get(typ).ref());return z.address()==0?false:x},
-})
-require('object.observe')
-
-Object.observe(Node, ( changes ) => {
-  console.log( "property "+change.name+" of " + change.object +" was "+change.type+ " to "+ change.object[change.name] );
-  if(change.name=="name") netbase.setName(change.object.id,change.object.name);// copy local string
-}) // multiple observers:
-
-netbase.query=(x)=>{
-  len=ref.alloc(int);
-  xs=netbase.execute(x,len);
-  xs.length=len.deref()
-  console.log("length:"+xs.length)
-  return xs.toArray().map(x=>x.deref())
+#!/usr/bin/env node
+require('./extensions.js')()
+try{
+  require('./netbase-ffi.js')
+  console.log("using native netbase-ffi.js")
+  return 
+}catch(ex){
+  console.log("Using online netbase fallback. Reason:")
+  console.log(ex.message)
 }
 
-// netbase.importTypes={all:'all',labels:'labels',wordnet:'wordnet',freebase:'freebase',geodb:'geodb',location:'geodb',dbpedia:'dbpedia',names:'names',images:'images',wikidata:'wikidata',yago:'yago',amazon:'amazon',test:'test',}
-netbase.help=()=>netbase.execute('help')
-netbase.importWordnet=()=>netbase.import('wordnet')
-netbase.server=()=>netbase.execute(":server")
+wget=url=>require('sync-request')('GET',url)
 
-// StructType.show=function(it) {console.log(it.deref())}
-netbase.show=function(it) {
-  if(!it)it="<null>"
-  else netbase.showNode(it.id)
-	if(it.type && it.type.toString()=='[StructType]')it=it.deref()
-	console.log(it)
+let cache = {}
+setEnglish=()=>{cache={};console.log("XDFDF");api = "http://netbase.pannous.com/json/all/"}
+setGerman=()=> {cache={};api = "http://de.netbase.pannous.com:81/json/all/"}
+
+if(process.env.NETBASE_LANGUAGE=="en") setEnglish()
+if(process.env.NETBASE_LANGUAGE=="de") setGerman()
+else setEnglish()
+
+api_limit = " limit 100"
+World = new Proxy({}, { get: (_, object) => getThe(object) });
+class Netbase {
+  get(id){return proxy(id)}
+}
+class Edges {
+  constructor(node) {
+    this.edges = node.statements
+    this.object = node
+  }
+  get first() { return this.edges[0] }
+  get last() { return this.edges[this.count - 1] }
+  get size() { return this.edges.length }
+  get count() { return this.edges.length }
+  toString() { return "dsaffa" }
+  get toString() { return "dsaffa" }
+
+}
+parse = (result) => {
+  // console.log(result)
+  // console.log(result.results[0].name)
+  return result.results[0]
 }
 
-netbase.shared_memory={}
-KB=1024
-MB=1024*1024
-GB=1024*1024*1024  
+getThe = function (name) {
+  // if (name == Symbol("util.inspect.custom"))return World //  name ="World"
+  if (typeof name == 'symbol')return World
+  // console.log(name);
+  if (name in cache) return cache[name]
+  return proxify(new Node(0, name).load())
+  // return fetch(api+name).then(response=>response.json().then(parse).then(create))
+}
 
-// ******************** INIT NEBASE !! *********************************
-old=process.cwd()
-process.chdir(process.env['HOME']+"/netbase")
-netbase.allowWipe()
-netbase.init(true)
-if(!netbase.hasNode('bug'))netbase.import.async('wordnet', () => {});
-process.chdir(old)
-// *********************************************************************
-
-// netbase.execute("help")
-netbase.learn("a.b=c")
-b=netbase.get('hi')
-
-// b=netbase.get(5)
-// netbase.show(b)
-// c=netbase.query("bug")
-// netbase.show(c)
-// netbase.testAll()
-
-function importAndDownload(type,file){
-  if(!type)type='all'
-    if(file)netbase.import(type,file)
-    if(!file){
-      system( __dirname+"/import/download-"+type+".sh")
-      netbase.import(type)
+function getProperties(node, property) {
+  property = property.sub(0,-1)
+  console.log('getProperties ' + node._name + ":" + property);
+  properties=[]
+  // console.log(node);
+  // console.log(node.edges);
+  for (edge of node.statements) {
+    if (edge.predicate.lower == property) {
+      // console.log(edge);
+      properties.add(edge.oid == node.id ? edge.subject : edge.object)
     }
+    if (edge.predicate.lower == property + " of") {
+      // console.log(edge);
+      properties.add(edge.oid == node.id ? edge.subject : edge.object) // of->inverse!
+    }
+  }
+  for (edge of node.statements) {
+    if (edge.predicate.lower.contains(property)) {
+      // console.log(edge);
+      properties.add( edge.oid == node.id ? edge.subject : edge.object)
+    }
+  }
+  return properties
 }
-// netbase.server()
-// if (fun_object.isNull()) 
-module.exports = {
-  get:netbase.get,
-  query:netbase.query,
-  test:netbase.test,
-  import:importAndDownload,
+
+
+/*     { id: 2233080, subject: 'Universe', predicate: 'type', object: 'Physical system', sid: 1, pid: -3, oid: 1454986 }, }, */
+function getProperty(node, property) {
+  console.log('getProperty ' + node._name + ":" + property);
+  // console.log(node);
+  // console.log(node.edges);
+  property = property.replace("_", " ").lower
+  // console.log(node);
+  for (edge of node.statements) {
+    if (edge.predicate.lower == property) {
+      // console.log(edge);
+      return edge.oid == node.id ? edge.subject : edge.object
+    }
+    if (edge.predicate.lower == property + " of") {
+      // console.log(edge);
+      return edge.oid == node.id ? edge.subject : edge.object
+      // return edge.oid == node.id ? edge.object : edge.subject // of->inverse!
+    }
+  }
+  for (edge of node.statements) {  
+    if (edge.predicate.lower.contains(property)) {
+      // console.log(edge);
+      return edge.oid == node.id ? edge.subject : edge.object
+    }
+  }
+  if (property.lower.endsWith("s"))return getProperties(node,property)
 }
+
+
+no_proxy = { lenght: 1, name: 1, inspect:1, edges:1}
+function proxify(object = {}) {
+  // return object
+  return new Proxy(object, {
+    get: (_, property) => {
+      proto = Object.getPrototypeOf(object)
+      if (typeof property == 'symbol' || property[0] == "_" || property in no_proxy) return object[property]
+      return object[property] || proto[property] || getProperty(object, property) || "missing :" + property
+    }
+  });
+}
+class Node {
+  constructor(id, name = 0) {
+    this.id = id
+    if (name) this.name = name
+  }
+  load() {
+    if(this._edges){
+      console.log("already loaded");
+      return this;
+    }
+    let endpoint = this.id ? api + this.id : api + String(this._name) + api_limit
+    // let result=fetch(endpoint).then(response=>response.json().then(parse))
+    console.log(endpoint);
+    var result;
+    let file = (this._name || this.id) + ".j5"
+    try {
+      result = read_json5(file)
+    } catch (x) {
+      if (exists(file)) console.log(x);
+      console.log(wget(endpoint))
+      let json=string(wget(endpoint).body)
+      console.log(json)
+      result = parse(JSON.parse(json))
+      try{
+        save_json5(file,result)
+      }catch(ex){console.log("can't cache "+file)}
+    }
+    Object.defineProperty(this, "_edges", { enumerable: false, value: new Edges(result) });
+    Object.defineProperty(this, "statements", { enumerable: false, value: result.statements });
+    delete result.statements // ^^
+    if (!result.description)delete result.description
+    if (result.description)this.description = result.description
+    // this.topic = result.topic || result.class
+    this.class = result.class || result.topic
+    proxy = proxify(this)
+  // node.kind=result.kind
+  // node.image=result.image
+  // node.statementCount=result.statementCount
+    // delete result.kind
+    // Object.assign(this,result)
+    if (!this.id) this.id = result.id
+    this._name = result.name
+    cache[this._name] = cache[this.id] = proxy
+    return proxy
+  }
+  browse() { open(api.replace("json", "html") + this.id) }
+
+  get predicates() { return this.statements.map(x => x.predicate).unique() }
+  get edges() {
+    console.log("edges");
+    if (!this._edges) this.load()
+    // this._edges=new Edges(this.load());
+    return this._edges
+  }
+  get name() { if (!this._name) this.load(); return this._name }
+  set name(n) { this._name = n }
+  show() {
+    console.log(this.id + "\t" + this.name);
+    for (var e of this.statements)
+      console.log(e.subject + "\t" + e.predicate + "\t" + e.object);
+  }
+  // get name() { if(!this.name)this.load();return this.name}
+  // Object.defineProperty(this, "name", { enumerable: false, value: n});
+}
+Node.prototype.getProperty = getProperty
+// Node.prototype.toString = function(){return "[object Foo]";}
+// n=new Node(1)
+function proxy(id) {
+  if (id in cache) return cache[id]
+  if (typeof id == 'string')
+    return getThe(id)
+  // return (new Node(id)) // .load() later!
+  proxy = proxify(new Node(id)) // .load() later!
+  cache[id] = proxy
+  // cache[node._name] = node  useless here
+  return proxy
+}
+
+
+
+module.exports = exports = {getThe:getThe, get:getThe,setGerman:setGerman,setEnglish:setEnglish}
